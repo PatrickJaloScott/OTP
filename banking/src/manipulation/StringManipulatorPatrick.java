2f889b93fd70be7dc07ebdfccea8fa679e2e240a
PatrickJaloScott <patrickjalo.scott@gmail.com>
2024-03-15 00:03:39 +0200

    Title: simplicity update * StringManipulatorPatrick.java: removed return variable

--- banking/src/manipulation/StringManipulatorPatrick.java
+++ banking/src/manipulation/StringManipulatorPatrick.java
@@ -2,13 +2,11 @@ package manipulation;
 
 public class StringManipulatorPatrick {
     public static String reverse(String input) {
-        String reversedString = "";
         StringBuilder reverser = new StringBuilder();
         for (int i = input.length()-1; i >= 0; i--) {
             reverser.append(input.charAt(i));
         }
-        reversedString = reverser.toString();
-        return reversedString;
+        return reverser.toString();
     }
 
     public static String capitalize(String input) {
